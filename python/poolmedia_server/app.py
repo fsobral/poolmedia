c8616f14d0423056cdcc2ac69e8340b159f06536
Francisco N. C. Sobral <fsobral@uem.br>
2021-01-18 11:56:02 -0300

    Comment database calls and change names.

--- python/poolmedia_server/app.py
+++ python/poolmedia_server/app.py
@@ -6,26 +6,26 @@ from flask_cors import CORS
 # Factory
 def create_app(ini_file):
 
-	app = Flask(__name__)
-	CORS(app)
+    app = Flask(__name__)
+    CORS(app)
 
-	config = ConfigParser()
-	config.read(ini_file)
+    config = ConfigParser()
+    config.read(ini_file)
 
-	__URL_PREFIX = config.get('ClassPack', 'url.prefix', fallback='/')
+    __URL_PREFIX = config.get('Poolmedia', 'url.prefix', fallback='/')
 
-	from optimizer import optimizer, config_optimizer
-	config_optimizer(config)
-	app.register_blueprint(optimizer, url_prefix=__URL_PREFIX)
+    from fortran_caller import optimizer, config_fortran_caller
+    config_fortran_caller(config)
+    app.register_blueprint(optimizer, url_prefix=__URL_PREFIX)
 
-	from database import init_db
-	init_db(config, app)
+    # from database import init_db
+    # init_db(config, app)
 
-	return app
+    return app
 
 
 if __name__ == '__main__':
 
-	app = create_app("./cp_config.ini")
+    app = create_app("./cp_config.ini")
 
-	app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
